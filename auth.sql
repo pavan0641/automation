b8c891233c1a617d9a8c1a897acf1f0b9cb680ac
pavan0641 <pavan0641@users.noreply.github.com>
2016-06-13 23:04:26 +0530

    Add files via upload

---
+++ auth.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS pavandatabase;
+
+USE pavandatabase;
+
+--
+-- Table structure for table `customers_auth`
+--
+
+CREATE TABLE IF NOT EXISTS `customers_auth` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=187 ;
